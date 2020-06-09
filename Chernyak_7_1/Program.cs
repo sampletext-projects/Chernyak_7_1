bbf5b46d0308b9be7ec094639d7e3891ce21282c
sampletext32 <sampletext32@bk.ru>
2020-06-09 21:30:24 +0300

    Add project

---
+++ Chernyak_7_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chernyak_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выполнила Черняк Оксана Александровна, АЭМ-111");
+            string debug = "i am as awesome as my ass";
+
+            Console.WriteLine("Введите строку для анализа, либо 'debug': ");
+            string text = Console.ReadLine();
+            if (text == "debug")
+            {
+                text = debug;
+                Console.WriteLine("Обработка ввода: \n" + debug);
+            }
+
+            Console.WriteLine("Введите символ для поиска: ");
+            char c = Console.ReadLine()[0];
+
+            int count = 0;
+            string[] tokens = text.Split(' ');
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i][0] == c)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("{0} слов начинается с символа '{1}'", count, c);
+
+            Console.ReadKey();
+        }
+    }
+}
